5dc4f4d95292c1d91be19593884bcbf86a675681
Lombra <Lombra@users.noreply.github.com>
2016-07-25 16:53:13 +0200

    - DualSpec renamed Spec and fixed.

---
+++ Spec.lua
@@ -0,0 +1,46 @@
+local _, core = ...
+
+local function onClick(self, spec)
+	SetSpecialization(spec)
+end
+
+local dropdown = core:CreateDropdown("Menu")
+dropdown.xOffset = 0
+dropdown.yOffset = 0
+dropdown.initialize = function(self, level)
+	local specIndex = GetSpecialization()
+	for i = 1, GetNumSpecializations() do
+		local id, name, _, icon = GetSpecializationInfo(i)
+		local info = UIDropDownMenu_CreateInfo()
+		info.text = name
+		info.func = onClick
+		info.arg1 = i
+		info.checked = (i == specIndex)
+		self:AddButton(info)
+	end
+end
+
+local module = core:NewModule("DualSpec", {
+	type = "data source",
+	label = "Spec",
+	OnClick = function(self)
+		dropdown:ToggleMenu(nil, self)
+	end,
+})
+
+function module:OnInitialize()
+	self:RegisterEvent("PLAYER_LOGIN", "Update")
+	self:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED", "Update")
+end
+
+function module:Update()
+	local spec = GetSpecialization()
+	if spec then
+		local _, name, _, icon = GetSpecializationInfo(spec)
+		self.text = name
+		self.icon = icon
+	else
+		self.text = NONE
+		self.icon = [[Interface\Icons\Ability_Marksmanship]]
+	end
+end
