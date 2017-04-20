319fe2cc31020f391dd933cb449eeb8e5213525e
Lombra <Lombra@users.noreply.github.com>
2017-04-20 12:19:39 +0200

    - C_UI.Reload.

--- ReloadUI.lua
+++ ReloadUI.lua
@@ -4,7 +4,7 @@ local module = core:NewModule("ReloadUI", {
 	type = "launcher",
 	label = "Reload UI",
 	icon = [[Interface\PaperDollInfoFrame\UI-GearManager-Undo]],
-	OnClick = ReloadUI,
+	OnClick = C_UI.Reload,
 	OnTooltipShow = function(self)
 		self:AddLine("Broker: ReloadUI", HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b)
 		self:AddLine("Click to reload UI.")
