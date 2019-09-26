4d7f9908a24733afa6c637d4bf94f22b4067b92b
Lombra <Lombra@users.noreply.github.com>
2019-09-26 15:35:38 +0200

    Simplify ReloadUI tooltip title.

--- ReloadUI.lua
+++ ReloadUI.lua
@@ -6,7 +6,7 @@ local module = core:NewModule("ReloadUI", {
 	icon = [[Interface\PaperDollInfoFrame\UI-GearManager-Undo]],
 	OnClick = C_UI.Reload,
 	OnTooltipShow = function(self)
-		self:AddLine("Broker: ReloadUI", HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b)
+		self:AddLine("ReloadUI", HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b)
 		self:AddLine("Click to reload UI.")
 	end
 })
