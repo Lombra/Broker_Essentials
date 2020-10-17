ce75fc25cf065271581c6e4d763b1b36ce7d4556
Lombra <Lombra@users.noreply.github.com>
2020-10-17 13:36:32 +0200

    - Update currency module for API changes.

--- Currency.lua
+++ Currency.lua
@@ -7,14 +7,14 @@ local module = core:NewModule("Currency", {
 	OnClick = function(self) ToggleCharacter("TokenFrame") end,
 	OnTooltipShow = function(self)
 		self:AddLine("Broker: Currency", HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b)
-		for i = 1, GetCurrencyListSize() do
-			local name, isHeader, isExpanded, isUnused, isWatched, count, icon = GetCurrencyListInfo(i)
-			if not isHeader then
-				self:AddDoubleLine(format("|T%s:0|t %s", icon, name), count, HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b, HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b)
+		for i = 1, C_CurrencyInfo.GetCurrencyListSize() do
+			local currencyInfo = C_CurrencyInfo.GetCurrencyListInfo(i)
+			if not currencyInfo.isHeader then
+				self:AddDoubleLine(format("|T%s:0|t %s", currencyInfo.iconFileID, currencyInfo.name), currencyInfo.quantity, HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b, HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b)
 			else
-				local nextName, nextIsHeader = GetCurrencyListInfo(i + 1)
-				if nextName and not nextIsHeader then
-					self:AddLine(name)
+				local nextCurrencyInfo = C_CurrencyInfo.GetCurrencyListInfo(i + 1)
+				if nextCurrencyInfo and not nextCurrencyInfo.isHeader then
+					self:AddLine(currencyInfo.name)
 				end
 			end
 		end
